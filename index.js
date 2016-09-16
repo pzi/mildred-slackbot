5172691d14b87f3d0b06ede516bf98e36afa37a3
Patrik Affentranger <patrik.affentranger@gmail.com>
2016-09-16 09:36:24 +0800

    Mildred no longer offers herself a drink

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var Mildred = require('./lib/mildred-bot');
 
 var mildredBot = new Mildred({
   token: process.env.SLACK_BOT_API_TOKEN,
-  name: 'Mildred Fry'
+  name: 'mildred' // user name not real name
 });
 
 mildredBot.run();
